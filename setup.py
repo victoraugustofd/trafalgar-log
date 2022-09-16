c49cb2bf79320c1b08cc99617dbb29bec8ae7961
victoraugustofd <victoraugustofd@gmail.com>
2022-09-16 19:10:22 -0300

    First commit.

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# Package version
+VERSION = "1.0.0"
+
+setup(
+    name="trafalgar-log",
+    version=VERSION,
+    description="A log framework that prints logs as JSON.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/victoraugustofd/trafalgar-log",
+    author="victoraugustofd",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: System :: Logging",
+    ],
+    keywords="logs",
+    packages=find_packages(where="src", exclude="tests"),
+    python_requires=">=3.7, <4",
+    install_requires=["dynaconf", "python-json-logger"],
+    license="MIT",
+)
