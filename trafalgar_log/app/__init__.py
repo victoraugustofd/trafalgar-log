7d831772ae42091f0c4a660ceef6a34ede2400e0
victoraugustofd <victoraugustofd@gmail.com>
2022-09-17 01:05:27 -0300

    Fixing gitignore and uploading project.

---
+++ trafalgar_log/app/__init__.py
@@ -0,0 +1,21 @@
+from dynaconf import Dynaconf, Validator, ValidationError
+
+from trafalgar_log.core.exceptions import ConfigurationError
+
+SETTINGS = Dynaconf(
+    envvar_prefix="LIB_LOG",
+    load_dotenv=True,
+    validators=[
+        Validator(
+            "APP_NAME",
+            "DOMAIN",
+            must_exist=True,
+        ),
+        Validator("LOG_LEVEL", default="INFO"),
+    ],
+)
+
+try:
+    SETTINGS.validators.validate_all()
+except ValidationError as e:
+    raise ConfigurationError()
