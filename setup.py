8b34895be4242cfac35459163e324efd94c33885
victoraugustofd <victoraugustofd@gmail.com>
2022-09-24 15:12:12 -0300

    Fixing error when log event comes from a python file outside all packages.

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ def _get_version() -> str:
 
 
 # Package version
-VERSION = _get_version()
+VERSION = "1.2.1"
 
 setup(
     name="trafalgar-log",
--- trafalgar_log/core/utils.py
+++ trafalgar_log/core/utils.py
@@ -71,12 +71,15 @@ def _find_relative_path(record: LogRecord) -> str:
     pathname = record.pathname.replace("/", os.sep)
     file_name = os.path.basename(record.filename)
 
-    return [
-        os.path.relpath(pathname, path)
-        for path in OS_PATHS
-        if pathname.startswith(path)
-        and file_name != os.path.relpath(pathname, path)
-    ][0]
+    try:
+        return next(
+            os.path.relpath(pathname, path)
+            for path in OS_PATHS
+            if pathname.startswith(path)
+            and file_name != os.path.relpath(pathname, path)
+        )
+    except StopIteration:
+        return record.filename
 
 
 def _get_code_line(record: LogRecord) -> str:
